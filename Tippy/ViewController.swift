daba18b2aa12d8d71e0cacb8e92e142613af9797
Ranjana Pokharana <pokharna89.ranjana@gmail.com>
2017-03-19 23:35:28 -0700

    Remember bill amount and Locale settings

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -22,7 +22,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        //show the decimal pad as soon as viewappear
         billField.becomeFirstResponder()
+        
+        //write the placeholder text
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        billField.attributedPlaceholder = NSAttributedString(string: currencyFormatter.string(from : 0)!)
+        
+        
         tipControl.selectedSegmentIndex = 0
         userDefaults = UserDefaults.standard
         defaultTip = userDefaults?.integer(forKey: "defaultTip")
@@ -31,7 +44,16 @@ class ViewController: UIViewController {
             if(defaultTip == tmp){
                 tipControl.selectedSegmentIndex = index
             }
-        }        
+        }
+        
+        //remember the bill from a recent* previous session (60*10 = 10 minutes)
+            if (userDefaults?.object(forKey: "default_bill") != nil) {
+                let now = NSDate()
+                let previous = userDefaults?.object(forKey: "time_at_close")!
+                if (now.timeIntervalSince(previous as! Date) < 60 * 10 ) {
+                    billField.text = userDefaults?.object(forKey: "default_bill") as! String!
+                }
+            }
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -42,6 +64,9 @@ class ViewController: UIViewController {
                 tipControl.selectedSegmentIndex = index
             }
         }
+        userDefaults?.set(billField.text, forKey: "default_bill")
+        userDefaults?.set(NSDate(),forKey : "time_at_close")
+        userDefaults?.synchronize()
         calculateBill(self)
     }
     override func viewDidAppear(_ animated: Bool) {
@@ -74,12 +99,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateBill(_ sender: Any) {
-        let bill = Double(billField.text!) ?? 0
-        let tip = bill * (Double)(tips[tipControl.selectedSegmentIndex])/100
-        let total = bill + tip
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        
+        // localize to your grouping and decimal separator
+        currencyFormatter.locale = Locale.current
+       
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = (bill * (Double)(tips[tipControl.selectedSegmentIndex])/100)
+        let tipValue = tip as NSNumber
+        let total = (bill + tip ) as NSNumber
+        
+        tipLabel.text = currencyFormatter.string(from: tipValue)
+        //String(format: "$%.2f", tip)
+        totalLabel.text = currencyFormatter.string(from: total)
+            //String(format: "$%.2f", total)
     }
     
     @IBAction func editingDidEnd(_ sender: Any) {
